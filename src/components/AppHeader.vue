b1ee33e4112d158f5103a65cbe616a17fb104a03
dublop <dnlopezavila@gmail.com>
2024-12-14 17:08:33 -0400

    create AppHeader component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <header>
+        <h1><span class="icon" aria-hidden="true">To do</span></h1>
+        <p>Lista de tareas en Vue.JS 3</p>
+    </header>
+</template>
+
+<style scoped>
+header {
+    border-bottom: #333 1px solid;
+    background-color: #fff;
+}
+header::after {
+    content: "";
+    display: block;
+    height: 1px;
+    box-shadow:  0 0 10px 0 rgb(0, 0,0.75);
+}
+h1 {
+    font-size: 2rem;
+}
+h1 .icon{
+    font-size: 1rem;
+    vertical-align: middle;
+}
+
+</style>
