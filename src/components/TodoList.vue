db29ca064330b3f4fb3627cb9dd638fdd56130b8
dublop <dnlopezavila@gmail.com>
2024-12-14 18:13:35 -0400

    primera version lista de tareas

---
+++ src/components/TodoList.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    import ListItem from "./ListItem.vue"
+</script>
+
+<template>
+    <ul>
+        <ListItem :isChecked="false">Esta es la tarea</ListItem>
+    </ul>
+</template>
+
+<style scoped>
+
